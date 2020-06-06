23f7ff4dec57f265003be9c58dfcd73622e3f208
wu <2295804511@qq.com>
2020-06-06 21:39:26 +0800

    HJ58_输入n个数，输出其中最小的k个

---
+++ src/problems/HJ58.java
@@ -0,0 +1,33 @@
+package problems;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @User: 吴广谋
+ * @Date: 2020/6/6
+ * @Description: 输入n个整数，输出其中最小的k个。
+ * 输入描述：第一行输入两个整数n、k，n为数组元素个数，k为需要输出的最小的个数      输出描述：输出一个整数数组
+ */
+public class HJ58 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()){
+            int n = input.nextInt();
+            int k = input.nextInt();
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                array[i] = input.nextInt();
+            }
+            Arrays.sort(array);
+            for (int i = 0; i < k; i++) {
+                if (i == k - 1){
+                    System.out.println(array[i]);
+                } else {
+                    System.out.print(array[i] + " ");
+                }
+            }
+        }
+    }
+}

eea36be616b4db0526ae0548085702a7c621c3e1
wu <2295804511@qq.com>
2020-06-29 18:01:56 +0800

    HJ14_字符串的连接最长路径查找

---
+++ src/problems/HJ14.java
@@ -0,0 +1,28 @@
+package problems;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @User: 吴广谋
+ * @Date: 2020/6/29
+ * @Description: 给定n个字符串，请对n个字符串按照字典序排列。
+ * 输入描述：输入第一行为一个正整数n(1≤n≤1000),下面n行为n个字符串(字符串长度≤100),字符串中只含有大小写字母。
+ * 输出描述：数据输出n行，输出结果为按照字典序排列的字符串。
+ */
+public class HJ14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            String str = input.next();
+            arr[i] = str;
+        }
+        Arrays.sort(arr);
+        for (String anArr : arr) {
+            System.out.println(anArr);
+        }
+    }
+}

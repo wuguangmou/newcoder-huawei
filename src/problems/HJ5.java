6bbb14c0934ffe521fd5bf6b1abaede1cb03e483
wu <2295804511@qq.com>
2020-06-29 11:34:59 +0800

    HJ5_进制转换

---
+++ src/problems/HJ5.java
@@ -0,0 +1,24 @@
+package problems;
+
+import java.util.Scanner;
+
+/**
+ * @User: 吴广谋
+ * @Date: 2020/6/29
+ * @Description: 写出一个程序，接受一个十六进制的数，输出该数值的十进制表示（多组同时输入）。
+ */
+public class HJ5 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNext()){
+            String hexNum = input.next();
+
+            //将字符串中的"0x"去掉
+            hexNum = hexNum.contains("0x") ? hexNum.replace("0x", "") : hexNum;
+            int num = Integer.valueOf(hexNum, 16);
+            System.out.println(num);
+        }
+    }
+}

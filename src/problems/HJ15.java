15a461da6faf785bb84209d96b6a1b37d092c95b
wu <2295804511@qq.com>
2020-07-02 14:39:45 +0800

    HJ15_求int型正整数在内存中存储时1的个数

--- src/problems/HJ15.java
+++ src/problems/HJ15.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
  * @Date: 2020/7/2
  * @Description: 输入一个int型的正整数，计算出该int型数据在内存中存储时1的个数。
  * 输入描述： 输入一个整数（int类型）      输出描述：这个数转换成2进制后，输出1的个数
+ * 例：输入：5       输出：2
  */
 public class HJ15 {
 
